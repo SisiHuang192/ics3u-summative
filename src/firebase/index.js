bbc4380da597a6911cea455cb7278b57b5a6c71c
sisi <sisi.huang@student.tdsb.on.ca>
2024-12-11 14:14:36 -0500

    have base code

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,6 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
+const firebaseConfig = {
+    apiKey: "AIzaSyDSalPKl9WqfuJiMPa8m6LkdELfhXHXuvA",
+    authDomain: "summative-472c8.firebaseapp.com",
+    projectId: "summative-472c8",
+    storageBucket: "summative-472c8.firebasestorage.app",
+    messagingSenderId: "481287341304",
+    appId: "1:481287341304:web:6e547d3d99815a1873a70d"
+  };
 
 
 const config = initializeApp(firebaseConfig)
